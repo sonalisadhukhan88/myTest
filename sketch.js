e687e7115d9d514d6194a64913cf4848c0ac35e6
sonali.sadhukhan88@gmail.com <sonali.sadhukhan88@gmail.com>
2020-12-07 23:01:58 +0530

    Added platform

--- sketch.js
+++ sketch.js
@@ -34,6 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    platform = new box1(100,100,400,10);
 
 }
 
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
